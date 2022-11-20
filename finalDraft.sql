83b3c48944164f2ee1700717cff4e7e19582c2ec
Matthew Graham <57013402+bugster2010@users.noreply.github.com>
2022-11-19 22:41:08 -0800

    Add files via upload

---
+++ finalDraft.sql
@@ -0,0 +1,80 @@
+CREATE TABLE Parks (
+    parkID INT PRIMARY KEY,
+    locationID INT,
+    parkName VARCHAR(100),
+    parksize INT,
+    parkCapacity INT,
+    numSectors INT,
+    isOpen BIT,
+
+    FOREIGN KEY (locationID) REFERENCES Sections(sectionID)
+);
+
+CREATE TABLE Sections (
+    sectionID INT PRIMARY KEY,
+    parkID INT,
+    sectionName VARCHAR(100),
+    sectionColorTheme VARCHAR(100),
+    numAttractions INT,
+
+    FOREIGN KEY (parkID) REFERENCES Parks(parkID)
+);
+
+CREATE TABLE Location (
+    locationID INT PRIMARY KEY,
+    locationName VARCHAR(100),
+    locationState VARCHAR(100),
+    locationCity VARCHAR(100)
+);
+
+CREATE TABLE Restaurants (
+    restID INT PRIMARY KEY,
+    sectionID INT,
+    restName VARCHAR(100),
+    restDescription VARCHAR(300),
+    restTypeFood VARCHAR(100),
+    isOpen BIT,
+    maxCapacity INT,
+    isFull BIT,
+    waitTime TIME,
+
+    FOREIGN KEY (sectionID) REFERENCES Sections(sectionID)
+);
+
+CREATE TABLE Rides (
+    rideID INT PRIMARY KEY,
+    sectionID INT,
+    rideName VARCHAR(100),
+    rideType VARCHAR(100),
+    rideDescription VARCHAR(300),
+    rideMinHeight DEC,
+    rideAvgAge INT,
+    rideOpeningYear INT,
+    waitTime DEC,
+
+    FOREIGN KEY (sectionID) REFERENCES Sections(sectionID)
+);
+
+CREATE TABLE Utilities (
+    utilityName VARCHAR(100) PRIMARY KEY,
+    sectionID INT,
+    description VARCHAR(300),
+    isAvailable BIT,
+
+    FOREIGN KEY (sectionID) REFERENCES Sections(sectionID)
+);
+
+CREATE TABLE Shops (
+    shopID INT PRIMARY KEY,
+    sectionID INT,
+    shopType VARCHAR(100),
+    shopName VARCHAR(100),
+    maxPrice DEC,
+    minPrice DEC,
+    avgPrice DEC,
+    numItems INT,
+    isOpen BIT,
+
+    FOREIGN KEY (sectionID) REFERENCES Sections(sectionID)
+)
+
