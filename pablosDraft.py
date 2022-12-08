3cfe153be8a48ee19561b60c630f367f25c800e5
Rodrigo Duran <rduran@chapman.edu>
2022-12-08 13:17:10 -0800

    added pablos frontedn

---
+++ pablosDraft.py
@@ -0,0 +1,86 @@
+# to run: go to project folder where Pipenv is and run: pipenv shell
+# then run: streamlit run myfile.py
+# to quit streamlit server: ctrl-c
+# to exit environment enter "exit"
+
+import streamlit as st
+
+st.set_page_config(page_title="Disney Dashboard",
+                    layout="wide")
+
+
+
+# ------------ SIDEBAR -------------
+
+st.sidebar.header("Editing Menu")
+addStuff = st.sidebar.button("Add to database")
+delStuff = st.sidebar.button("Delete from database")
+modStuff = st.sidebar.button("Update database")
+
+# ----- MAIN PAGE -----
+st.title("Disney Dashboard")
+st.markdown("##")
+
+with st.expander("Filter data"):
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        park = st.multiselect(
+            "Park:",
+            options="M",
+            default = "M"
+        )
+        section = st.multiselect(
+            "Section:",
+            options="M",
+            default = "M"
+        )
+        location = st.multiselect(
+            "Location:",
+            options="M",
+            default = "M"
+        )
+    with col2:
+        restaurant = st.multiselect(
+            "Restaurant:",
+            options="M",
+            default = "M"
+        )
+        ride = st.multiselect(
+            "Ride:",
+            options="M",
+            default = "M"
+        )
+        utilities = st.multiselect(
+            "Utilities:",
+            options="M",
+            default = "M"
+        )
+    with col3:
+        shops = st.multiselect(
+            "Shop:",
+            options="M",
+            default = "M"
+        )
+        shops = st.multiselect(
+            "Filter:",
+            options="M",
+            default = "M"
+        )
+        shops = st.multiselect(
+            "Filter2:",
+            options="M",
+            default = "M"
+        )
+
+
+st.write("Table here at some point smh")
+
+if addStuff:
+    st.write("Enter info below to add to table")
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        s1 = st.text_input("Enter park")
+    with col2:
+        s2 = st.text_input("Enter section")
+    with col3:
+        s3 = st.text_input("Enter location")
