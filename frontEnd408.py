9e331c4562b433f0032009b1eb7bd08f2bd05c8c
Pablo Labbate <66883135+unstoppablo@users.noreply.github.com>
2022-12-08 14:17:18 -0800

    Add files via upload

---
+++ frontEnd408.py
@@ -0,0 +1,98 @@
+# to run: go to project folder where Pipenv is and run: pipenv shell
+# then run: streamlit run myfile.py
+# to quit streamlit server: ctrl-c
+# to exit environment enter "exit"
+# to be able to interact with dataframe: pip install streamlit-aggrid
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
+
+
+st.set_page_config(page_title="Disney Dashboard",
+                    layout="wide")
+
+
+
+# ------------ SIDEBAR -------------
+
+st.sidebar.header("Editing Menu")
+addStuff = st.sidebar.button("Add to database")
+delStuff = st.sidebar.button("Delete from database")
+modStuff = st.sidebar.button("Update database")
+
+# ----- MAIN PAGE -----
+st.title("Disney Dashboard")
+st.markdown("##")
+
+with st.expander("Filter data"):
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        park = st.multiselect(
+            "Park:",
+            options="M",
+            default = "M"
+        )
+        section = st.multiselect(
+            "Section:",
+            options="M",
+            default = "M"
+        )
+        location = st.multiselect(
+            "Location:",
+            options="M",
+            default = "M"
+        )
+    with col2:
+        restaurant = st.multiselect(
+            "Restaurant:",
+            options="M",
+            default = "M"
+        )
+        ride = st.multiselect(
+            "Ride:",
+            options="M",
+            default = "M"
+        )
+        utilities = st.multiselect(
+            "Utilities:",
+            options="M",
+            default = "M"
+        )
+    with col3:
+        shops = st.multiselect(
+            "Shop:",
+            options="M",
+            default = "M"
+        )
+        shops = st.multiselect(
+            "Filter:",
+            options="M",
+            default = "M"
+        )
+        shops = st.multiselect(
+            "Filter2:",
+            options="M",
+            default = "M"
+        )
+
+
+st.write("Table here at some point smh")
+# ------------- DATAFRAME ---------
+data = pd.read_csv("main_Sections.csv", index_col=0)
+AgGrid(data)
+# ---------------------------------
+
+if addStuff:
+    st.write("Enter info below to add to table")
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        s1 = st.text_input("Enter park")
+    with col2:
+        s2 = st.text_input("Enter section")
+    with col3:
+        s3 = st.text_input("Enter location")
+
+
---
+++ sqlconnector.py
@@ -0,0 +1,110 @@
+from select import select
+import sqlite3
+from sqlite3 import Error
+import pandas as pd
+
+
+
+# connecting to db
+def create_connection(db_fn):
+    """ create a database connection to a SQLite database """
+    conn = None # so our program doesn't crash later on if the connection was unsuccessful
+    try:
+        conn = sqlite3.connect(db_fn) # the actual connection step 
+        print(sqlite3.version) # to check the version, not necessary 
+        print ("Opened database successfully") 
+        
+    except Error as e:
+            print(e) # if something goes wrong, tell the user what happened
+
+    return conn
+
+def add_park(conn, project):
+    """
+    Create a new project into the projects table
+    :param conn:
+    :param project:
+    :return: project id
+    """
+    sql = ''' INSERT INTO Parks(parkID,locationID,parkName,parksize,parkCapacity,numSectors,isOpen)
+              VALUES(?,?,?,?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, project)
+    conn.commit()
+
+def add_section(conn, project):
+    """
+    Create a new project into the projects table
+    :param conn:
+    :param project:
+    :return: project id
+    """
+    sql = ''' INSERT INTO Sections(sectionID,parkID,sectionName,sectionColorTheme,numAttractions)
+              VALUES(?,?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, project)
+    conn.commit()
+
+def add_location(conn, project):
+    """
+    Create a new project into the projects table
+    :param conn:
+    :param project:
+    :return: project id
+    """
+    sql = ''' INSERT INTO Locations(locationID,locationName,locationState,locationCity)
+              VALUES(?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, project)
+    conn.commit()
+
+def add_restaurant(conn, project):
+    """
+    Create a new project into the projects table
+    :param conn:
+    :param project:
+    :return: project id
+    """
+    sql = ''' INSERT INTO Restaurant(restID,sectionID,parkID,restName,restDescription,restTypeFood,restTypeService,isOpen,maxCapacity,isFull,waitTime)
+              VALUES(?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, project)
+    conn.commit()
+# MISSING: add rides, utilities, shops
+
+
+# QUERY ALL DATA
+def select_all_accounts(conn):
+    """
+    Query tasks by priority
+    :param conn: the Connection object
+    :param priority:
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Sections")
+
+    rows = cur.fetchall() # to get the data back into a list 
+
+    for row in rows:
+        print(row)
+
+
+
+def main():
+		# create connection to our sqlite db file 
+    conn = create_connection(r"finalDraft.sql")
+
+    if conn:
+        try:
+            select_all_accounts(conn)
+            # result_df = pd.read_sql(select_all_accounts(conn), conn)`
+        finally:
+            conn.close()
+            print("connection closed")
+        
+
+
+
+if __name__ == '__main__':
+    main()
