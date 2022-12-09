3e3b37a5715879b0bbc309b47ee6d7c15358a991
Matthew Graham <bugster2010@yahoo.com>
2022-12-08 17:22:05 -0800

    fixing and reorganizing

--- finalDraft.sql
+++ finalDraft.sql
@@ -1,3 +1,10 @@
+DROP TABLE Parks;
+DROP TABLE Sections;
+DROP TABLE Location;
+DROP TABLE Restaurants;
+DROP TABLE Rides;
+DROP TABLE Utilities;
+
 CREATE TABLE Parks (
     parkID INT PRIMARY KEY,
     locationID INT,
@@ -7,7 +14,7 @@ CREATE TABLE Parks (
     numSectors INT,
     isOpen BIT,
 
-    FOREIGN KEY (locationID) REFERENCES Sections(sectionID)
+    FOREIGN KEY (locationID) REFERENCES Location(locationID)
 );
 
 CREATE TABLE Sections (
@@ -17,12 +24,9 @@ CREATE TABLE Sections (
     sectionColorTheme VARCHAR(100),
     numAttractions INT,
 
-    PRIMARY KEY (sectionID),
     FOREIGN KEY (parkID) REFERENCES Parks(parkID)
 );
 
---if needed
---DROP TABLE Sections;
 
 CREATE TABLE Location (
     locationID INT PRIMARY KEY,
@@ -32,7 +36,7 @@ CREATE TABLE Location (
 );
 
 CREATE TABLE Restaurants (
-    restID INT,
+    restID INT PRIMARY KEY,
     sectionID INT,
     restName VARCHAR(100),
     restDescription VARCHAR(300),
@@ -42,12 +46,9 @@ CREATE TABLE Restaurants (
     maxCapacity INT,
     waitTime INT,
 
-    PRIMARY KEY (restID),
-    FOREIGN KEY (sectionID) REFERENCES Sections(sectionID),
+    FOREIGN KEY (sectionID) REFERENCES Sections(sectionID)
 );
 
---if needed
-DROP TABLE Restaurants;
 
 CREATE TABLE Rides (
     rideID INT,
@@ -60,11 +61,8 @@ CREATE TABLE Rides (
     waitTime DEC,
 
     PRIMARY KEY (rideID),
-    FOREIGN KEY (sectionID) REFERENCES Sections(sectionID),
+    FOREIGN KEY (sectionID) REFERENCES Sections(sectionID)
 );
-
---if needed
-DROP TABLE Rides;
 
 CREATE TABLE Utilities (
     utilityID INT,
@@ -74,11 +72,8 @@ CREATE TABLE Utilities (
     isAvailable BIT,
 
     PRIMARY KEY (utilityID),
-    FOREIGN KEY (sectionID) REFERENCES Sections(sectionID),
+    FOREIGN KEY (sectionID) REFERENCES Sections(sectionID)
 );
-
---if needed
-DROP TABLE Utilities;
 
 CREATE TABLE Shops (
     shopID INT,
@@ -92,13 +87,8 @@ CREATE TABLE Shops (
     isOpen BIT,
 
     PRIMARY KEY (shopID),
-    FOREIGN KEY (sectionID) REFERENCES Sections(sectionID),
+    FOREIGN KEY (sectionID) REFERENCES Sections(sectionID)
 );
-
---if needed
-DROP TABLE Shops;
-
-
 
 -- Making these views
 
@@ -123,6 +113,11 @@ ORDER BY avgPrice ASC;
 
 
 -- Add records
+
+-- Location
+INSERT INTO Location(locationID, locationName, locationState, locationCity)
+VALUES (1, 'Disneyland Resort', 'California', 'Anaheim');
+
 -- PARKS --
 
     --Disneyland
@@ -176,53 +171,53 @@ VALUES(2, 2, "BBQ", "South Seas Trader", 13, 4, 7, 15, 1);
 
 -- RESTAURANTS --
     --Disneyland
-INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, isFull, waitTime)
-VALUES(1, 1,"Jolly Holiday Bakery Cafe", "In the morning, stop by Jolly Holiday Bakery Cafe on your way into Disneyland for fresh pastries, specialty coffees and a great view.", "Counter Service", "Cafe", 1, 190, 1, 45);
+INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, waitTime)
+VALUES(1, 1,"Jolly Holiday Bakery Cafe", "In the morning, stop by Jolly Holiday Bakery Cafe on your way into Disneyland for fresh pastries, specialty coffees and a great view.", "Counter Service", "Cafe", 1, 190, 45);
 
-INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, isFull, waitTime)
-VALUES(2, 1,"Gibson Girl Ice Cream Parlor", "Get a delicious ice cream cone, sundae, or float and relax in the old-fashioned ice cream parlor..", "Counter Service", "Ice cream", 1, 40, 0, 10);
+INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, waitTime)
+VALUES(2, 1,"Gibson Girl Ice Cream Parlor", "Get a delicious ice cream cone, sundae, or float and relax in the old-fashioned ice cream parlor..", "Counter Service", "Ice cream", 1, 40, 10);
 
-INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, isFull, waitTime)
-VALUES(3, 2,"Bengal Barbecue", "Located across from the Jungle Cruise and Indiana Jones Adventure, this quick serve location features chicken and beef skewers topped with Polynisian-inspired sauces.", "Counter Service", "BBQ", 1, 75, 1, 25);
+INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, waitTime)
+VALUES(3, 2,"Bengal Barbecue", "Located across from the Jungle Cruise and Indiana Jones Adventure, this quick serve location features chicken and beef skewers topped with Polynisian-inspired sauces.", "Counter Service", "BBQ", 1, 75, 25);
 
-INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, isFull, waitTime)
-VALUES(4, 2,"Tiki Juice Bar", "You can get refreshing Dole pineapple juice or Dole Whip (soft serve ice cream) here. This is located at the entrance to the Tiki Room. During busier times, the Tiki Juice Bar becomes Mobile Order only. Dole Whips can also be found at Tropical Hideaway.", "Counter Service", "Juice Bar", 1, 20, 0, 0);
+INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, waitTime)
+VALUES(4, 2,"Tiki Juice Bar", "You can get refreshing Dole pineapple juice or Dole Whip (soft serve ice cream) here. This is located at the entrance to the Tiki Room. During busier times, the Tiki Juice Bar becomes Mobile Order only. Dole Whips can also be found at Tropical Hideaway.", "Counter Service", "Juice Bar", 1, 20, 0);
 
-INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, isFull, waitTime)
-VALUES(5, 2,"South Sea Traders", "Located near the Bengal Barbecue, this fruit stand offers in-season fresh fruit, soft drinks and bottles of water.", "Quick Snacks", "Snacks", 1, 20, 1, 15);
+INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, waitTime)
+VALUES(5, 2,"South Sea Traders", "Located near the Bengal Barbecue, this fruit stand offers in-season fresh fruit, soft drinks and bottles of water.", "Quick Snacks", "Snacks", 1, 20, 15);
 
-INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, isFull, waitTime)
-VALUES(6, 2,"The Tropical Hideaway", "Formerly Aladdin''s Oasis. Visit with Jungle Cruise skippers as you sit on an open-air dock and enjoy tropical music. At night, torches light the area.", "N/A", "Fruits", 1, 40, 1, 10);
+INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, waitTime)
+VALUES(6, 2,"The Tropical Hideaway", "Formerly Aladdin''s Oasis. Visit with Jungle Cruise skippers as you sit on an open-air dock and enjoy tropical music. At night, torches light the area.", "N/A", "Fruits", 1, 40, 10);
 
-INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, isFull, waitTime)
-VALUES(7, 3,"Blue Bayou Restaurant", "For a special treat, have lunch or dinner at the Blue Bayou Restaurant. This restaurant is located inside the Pirates of the Caribbean attraction, and features appetizers, salads, seafood, chicken & beef dishes as well as its famous Monte Cristo sandwich, all with a New Orleans flair. This restaurant is a little pricey, but has a great atmosphere. Limited same day reservations are taken at the entrance to the restaurant, however advance reservations are recommended.", "Full Service","Full Menu", 1, 40, 1, 40);
+INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, waitTime)
+VALUES(7, 3,"Blue Bayou Restaurant", "For a special treat, have lunch or dinner at the Blue Bayou Restaurant. This restaurant is located inside the Pirates of the Caribbean attraction, and features appetizers, salads, seafood, chicken & beef dishes as well as its famous Monte Cristo sandwich, all with a New Orleans flair. This restaurant is a little pricey, but has a great atmosphere. Limited same day reservations are taken at the entrance to the restaurant, however advance reservations are recommended.", "Full Service","Full Menu", 1, 40, 40);
 
-INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, isFull, waitTime)
-VALUES(8, 3,"French Market Restaurant", "French Market has covered patio and features southern-themed dishes, including jambalaya, roasted chicken, salmon, and delicious desserts. On weekend afternoons, a jazz band sometimes entertains the guests while they eat.", "Cafeteria","Cafe", 1, 35, 0, 5);
+INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, waitTime)
+VALUES(8, 3,"French Market Restaurant", "French Market has covered patio and features southern-themed dishes, including jambalaya, roasted chicken, salmon, and delicious desserts. On weekend afternoons, a jazz band sometimes entertains the guests while they eat.", "Cafeteria","Cafe", 1, 35, 5);
 
-INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, isFull, waitTime)
-VALUES(9, 4,"Hungry Bear Restaurant", "This large restaurant offers American type fare for lunch or dinner, such as chicken sandwiches, cheeseburgers, and fries. You can sit above, or downstairs near the river and watch the boats and canoes go by. Kids meals are also available.", "Counter Service","American", 1, 50, 0, 0);
+INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, waitTime)
+VALUES(9, 4,"Hungry Bear Restaurant", "This large restaurant offers American type fare for lunch or dinner, such as chicken sandwiches, cheeseburgers, and fries. You can sit above, or downstairs near the river and watch the boats and canoes go by. Kids meals are also available.", "Counter Service","American", 1, 50, 0);
 
-INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, isFull, waitTime)
-VALUES(10, 4,"Harbour Galley", "This restaurant on the banks of the Rivers of America offers chowder and soups served in bread bowls, plus entree sized salads, baked potatoes and drinks.", "Counter service", "Healthy", 1, 20, 0, 20);
+INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, waitTime)
+VALUES(10, 4,"Harbour Galley", "This restaurant on the banks of the Rivers of America offers chowder and soups served in bread bowls, plus entree sized salads, baked potatoes and drinks.", "Counter service", "Healthy", 1, 20, 20);
 
-INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, isFull, waitTime)
-VALUES(11, 5,"Rancho del Zocalo Restaurante’", "Dine on classic Mexican specialties for lunch or dinner as you bask in the romance and heritage of early California. Menu items include burritos, nachos and much more. You can sit outside on the Mexican themed patio, which is partially covered.", "Cafeteria", "Mexican", 1, 200, 0, 50);
+INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, waitTime)
+VALUES(11, 5,"Rancho del Zocalo Restaurante’", "Dine on classic Mexican specialties for lunch or dinner as you bask in the romance and heritage of early California. Menu items include burritos, nachos and much more. You can sit outside on the Mexican themed patio, which is partially covered.", "Cafeteria", "Mexican", 1, 200, 50);
 
-INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, isFull, waitTime)
-VALUES(12, 5,"River Belle Terrace", "Eat lunch or dinner indoors or out on the terrace overlooking the Rivers of America. Options include BBQ ribs, oven-roasted BBQ chicken or a tasty brisket sandwich. Breakfast is available each morning.", "Full service", "BBQ", 0, 70, 0, 0);
+INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, waitTime)
+VALUES(12, 5,"River Belle Terrace", "Eat lunch or dinner indoors or out on the terrace overlooking the Rivers of America. Options include BBQ ribs, oven-roasted BBQ chicken or a tasty brisket sandwich. Breakfast is available each morning.", "Full service", "BBQ", 0, 70, 0);
 
-INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, isFull, waitTime)
-VALUES(13, 6,"Red Rose Taverne", "This little restaurant is Beauty and the Beast themed, and serves French-inspired flatbreads, burgers, and sandwiches. It is located on the backside of Fantasyland near the Dumbo attraction. Breakfast, Lunch and Dinner.", "Counter Service", "French", 1, 100, 1, 35);
+INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, waitTime)
+VALUES(13, 6,"Red Rose Taverne", "This little restaurant is Beauty and the Beast themed, and serves French-inspired flatbreads, burgers, and sandwiches. It is located on the backside of Fantasyland near the Dumbo attraction. Breakfast, Lunch and Dinner.", "Counter Service", "French", 1, 100, 35);
 
-INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, isFull, waitTime)
-VALUES(14, 7,"Docking Bay 7 Food and Cargo", "Docking Bay 7 Food and Cargo is where Chef Strono “Cookie” Tuggs is serving up his grub for weary travelers. Look for exotic and unusual dishing like smoked Kaadu ribs, Fried Endorian Tip-yip and Batuubon for dessert.", "N/A", "Exotic", 1, 40, 1, 80);
+INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, waitTime)
+VALUES(14, 7,"Docking Bay 7 Food and Cargo", "Docking Bay 7 Food and Cargo is where Chef Strono “Cookie” Tuggs is serving up his grub for weary travelers. Look for exotic and unusual dishing like smoked Kaadu ribs, Fried Endorian Tip-yip and Batuubon for dessert.", "N/A", "Exotic", 1, 40, 80);
 
-INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, isFull, waitTime)
-VALUES(15, 8,"Pluto''s Dog House", "Hot dogs and cold beverages. Children''s meals. Outdoor seating is available.", "Fast Fod", "Lunch, Dinner, Snack", 1, 20, 0, 20);
+INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, waitTime)
+VALUES(15, 8,"Pluto''s Dog House", "Hot dogs and cold beverages. Children''s meals. Outdoor seating is available.", "Fast Fod", "Lunch, Dinner, Snack", 1, 20, 20);
 
-INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, isFull, waitTime)
-VALUES(16, 9,"Alien Pizza Planet", "This Toy Story-themed restaurant offers pizza, Caesar salad, and pasta. You can eat inside, or “outside” in a covered area.", "Pizza", "Cafeteria", 1, 100, 1, 20);
+INSERT INTO Restaurants(restID, sectionID, restName, restDescription, restTypeFood, restTypeService, isOpen, maxCapacity, waitTime)
+VALUES(16, 9,"Alien Pizza Planet", "This Toy Story-themed restaurant offers pizza, Caesar salad, and pasta. You can eat inside, or “outside” in a covered area.", "Pizza", "Cafeteria", 1, 100, 20);
 
 
 
